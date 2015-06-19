caabd7d9aae3222b30eca27fc3f3af22acaf17fa
EloiZ <eloi.zablocki@gmail.com>
2015-06-19 05:47:19 -0400

    Fix a few bugs

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from build_model import build_model
 from dataset import get_minibatch_char
 from train import train_model
-from parser import parse_args
+from utils import parse_args
 
 if __name__ == "__main__":
     args = parse_args()
     dataset = args.dataset
 
-    if dataset == "wikipedia":
-        vocab_size = 205
-    elif dataset == "penntree":
-        vocab_size = 50
-
     mini_batch_size = args.mini_batch_size
     time_length = args.time_length
 
-    train_stream, valid_stream = get_minibatch_char(dataset, mini_batch_size,
-                                                    time_length)
+    # Prepare data
+    (train_stream,
+     valid_stream,
+     vocab_size) = get_minibatch_char(dataset, mini_batch_size, time_length)
+
+    # Build the model
     cost, cross_entropy = build_model(vocab_size, args)
+
+    # Train the model
     train_model(cost, cross_entropy, train_stream, valid_stream, args)
--- train.py
+++ train.py
@@ -13,7 +13,8 @@ from blocks.extensions.saveload import Checkpoint
 from blocks.graph import ComputationGraph
 from blocks.main_loop import MainLoop
 from blocks.model import Model
-from extensions import EarlyStopping
+# TODO Change the "Dump" in this extensions as it has been moved in blocks
+# from extensions import EarlyStopping
 
 floatX = theano.config.floatX
 logging.basicConfig(level='INFO')
@@ -59,7 +60,7 @@ def train_model(cost, cross_entropy, train_stream, valid_stream, args):
             DataStreamMonitoring([cost, cross_entropy],
                                  valid_stream, prefix='valid'),
             Checkpoint(args.save_path, after_epoch=True),
-            EarlyStopping(cost, args.patience, args.save_path)
+            # EarlyStopping(cost, args.patience, args.save_path),
             Printing(),
         ]
     )
