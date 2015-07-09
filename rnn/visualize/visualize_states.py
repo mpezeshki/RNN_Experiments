4a3044d346f160c7f3eaa3f70c403ada8d5a9ff4
EloiZ <eloi.zablocki@gmail.com>
2015-07-09 12:33:43 -0400

    Clean visualize states

--- rnn/visualize/visualize_states.py
+++ rnn/visualize/visualize_states.py
@@ -1,4 +1,5 @@
 import logging
+import re
 
 import numpy as np
 
@@ -14,41 +15,19 @@ logging.basicConfig(level='INFO')
 logger = logging.getLogger(__name__)
 
 
-def visualize_states(cost, updates,
+def visualize_states(cost, hidden_states, updates,
                      train_stream, valid_stream,
                      args):
 
-    # Time length of the visualization
-    text_length = args.visualize_length
+    # Get all the hidden_states
+    all_states = [
+        var for var in hidden_states if re.match("hidden_state_.*", var.name)]
+    all_states = sorted(all_states, key=lambda var: var.name[-1])
 
-    # Boolean to know if there are several states to see
-    see_several_states = (args.layers > 1 and (
-        args.skip_connections or args.skip_output)) or (
-        args.rnn_type in ["soft", "hard", "clockwork"])
-
-    # Get the hidden_state
-    name = "hidden_state"
-    variables = ComputationGraph(cost).variables
-
-    outputs = [var for var in variables
-               if hasattr(var.tag, 'name') and
-               name == var.name]
-
-    assert len(outputs) == 1
-
-    if see_several_states:
-        h = []
-        dim = args.state_dim
-        for i in range(args.layers):
-            h.append(
-                outputs[0][:, :, dim * i: dim * (i + 1)])
-    else:
-        h = outputs
-
-    cg = ComputationGraph(h)
-
-    assert(len(cg.inputs) == 1)
-    assert(cg.inputs[0].name == "features")
+    # Get all the hidden_cells
+    all_cells = [var for var in hidden_states if re.match(
+        "hidden_cell_.*", var.name)]
+    all_cells = sorted(all_cells, key=lambda var: var.name[-1])
 
     # Handle the theano shared variables for the state
     state_vars = [theano.shared(
@@ -58,25 +37,33 @@ def visualize_states(cost, updates,
     f_updates = [(x, upd) for x, (_, upd) in zip(state_vars, updates)]
 
     # Compile the function
-    compiled = theano.function(inputs=cg.inputs, outputs=h,
-                               givens=givens, updates=f_updates,
-                               mode='FAST_COMPILE')
+    logger.info("The compilation of the function has started")
+    if args.rnn_type == "lstm" and args.visualize_cells:
+        compiled = theano.function(inputs=ComputationGraph(all_cells).inputs,
+                                   outputs=all_cells,
+                                   givens=givens, updates=f_updates,
+                                   mode='FAST_COMPILE')
+    else:
+        compiled = theano.function(inputs=ComputationGraph(all_states).inputs,
+                                   outputs=all_states,
+                                   givens=givens, updates=f_updates,
+                                   mode='FAST_COMPILE')
 
-    epoch_iterator = valid_stream.get_epoch_iterator()
+    epoch_iterator = train_stream.get_epoch_iterator()
     for _ in range(10):
         init_ = next(epoch_iterator)[0][
-            0: text_length, 0:1]
+            0: args.visualize_length, 0:1]
 
         hidden_state = compiled(init_)
 
         layers = len(hidden_state)
         time = hidden_state[0].shape[0]
 
-        for i in range(layers):
-            plt.subplot(layers, 1, i + 1)
+        for d in range(layers):
+            plt.subplot(layers, 1, d + 1)
             for j in range(args.state_dim):
-                plt.plot(np.arange(time), hidden_state[i][:, 0, j])
-            plt.xticks(range(text_length), tuple(init_[:, 0]))
+                plt.plot(np.arange(time), hidden_state[d][:, 0, j])
+            plt.xticks(range(args.visualize_length), tuple(init_[:, 0]))
             plt.grid(True)
-            plt.title("hidden_state_of_layer_" + str(i))
+            plt.title("hidden_state_of_layer_" + str(d))
         plt.show()
