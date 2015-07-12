57346b23a64bdc0d1716cbd7d6fc1dfe1c17c218
Mohammad Pezeshki <mohammadpz@gmail.com>
2015-07-12 18:40:06 -0400

    unified

--- rnn/visualize/visualize_matrices.py
+++ rnn/visualize/visualize_matrices.py
@@ -1,24 +1,37 @@
+import logging
+
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import pylab
+
 from blocks.serialization import load_parameter_values
 
-path = "/media/win/Users/Eloi/new_toy_05_40_simple_4l_5units/best"
+import matplotlib
+# Force matplotlib to not use any Xwindows backend.
+matplotlib.use('Agg')
+from matplotlib import pylab
 
-param_values = load_parameter_values(path)
-print param_values.keys()
+logging.basicConfig(level='INFO')
+logger = logging.getLogger(__name__)
 
-input0 = param_values["/fork/fork_inputs/lookuptable.W_lookup"]
-input1 = param_values["/fork/fork_inputs_1/lookuptable.W_lookup"]
-input2 = param_values["/fork/fork_inputs_2/lookuptable.W_lookup"]
-input3 = param_values["/fork/fork_inputs_3/lookuptable.W_lookup"]
 
-inputall = np.concatenate((input0, input1, input2, input3), axis=1)
+def visualize_matrices(args):
 
-outputall = param_values["/output_layer.W"]
+    param_values = load_parameter_values(args.save_path + "/best")
+    print param_values.keys()
 
-pylab.matshow(inputall, cmap=pylab.cm.gray)
-pylab.matshow(outputall, cmap=pylab.cm.gray)
-pylab.matshow(param_values["/output_layer.b"][None, :], cmap=pylab.cm.gray)
+    input0 = param_values["/fork/fork_inputs/lookuptable.W_lookup"]
+    input1 = param_values["/fork/fork_inputs_1/lookuptable.W_lookup"]
+    input2 = param_values["/fork/fork_inputs_2/lookuptable.W_lookup"]
+    input3 = param_values["/fork/fork_inputs_3/lookuptable.W_lookup"]
 
-pylab.show()
+    inputall = np.concatenate((input0, input1, input2, input3), axis=1)
+
+    outputall = param_values["/output_layer.W"]
+
+    pylab.matshow(inputall, cmap=pylab.cm.gray)
+    pylab.matshow(outputall, cmap=pylab.cm.gray)
+    pylab.matshow(param_values["/output_layer.b"][None, :], cmap=pylab.cm.gray)
+
+    pylab.tight_layout()
+    pylab.savefig(args.save_path + "/visualize_matrices.png")
+    logger.info("Figure \"visualize_matrices"
+                ".png\" saved at directory: " + args.save_path)
