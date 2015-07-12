7c43a65cb7c9ab200db5242b87d02f709f8e7539
Mohammad Pezeshki <mohammadpz@gmail.com>
2015-07-12 19:03:46 -0400

    unified

--- rnn/visualize/visualize_matrices.py
+++ rnn/visualize/visualize_matrices.py
@@ -7,7 +7,7 @@ from blocks.serialization import load_parameter_values
 import matplotlib
 # Force matplotlib to not use any Xwindows backend.
 matplotlib.use('Agg')
-from matplotlib import pylab
+import matplotlib.pyplot as plt
 
 logging.basicConfig(level='INFO')
 logger = logging.getLogger(__name__)
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 
 def visualize_matrices(args):
 
-    param_values = load_parameter_values(args.save_path + "/best")
+    param_values = load_parameter_values(args.load_path)
     print param_values.keys()
 
     input0 = param_values["/fork/fork_inputs/lookuptable.W_lookup"]
@@ -23,15 +23,14 @@ def visualize_matrices(args):
     input2 = param_values["/fork/fork_inputs_2/lookuptable.W_lookup"]
     input3 = param_values["/fork/fork_inputs_3/lookuptable.W_lookup"]
 
-    inputall = np.concatenate((input0, input1, input2, input3), axis=1)
+    inputall = np.abs(np.concatenate((input0, input1, input2, input3), axis=1))
 
-    outputall = param_values["/output_layer.W"]
+    outputall = np.abs(param_values["/output_layer.W"])
 
-    pylab.matshow(inputall, cmap=pylab.cm.gray)
-    pylab.matshow(outputall, cmap=pylab.cm.gray)
-    pylab.matshow(param_values["/output_layer.b"][None, :], cmap=pylab.cm.gray)
+    # plt.matshow(inputall / np.mean(inputall), cmap=plt.cm.gray)
+    # plt.matshow(outputall / np.mean(outputall), cmap=plt.cm.gray)
+    plt.plot(np.arange(outputall.shape[0]), np.sum(np.abs(outputall), axis=1))
 
-    pylab.tight_layout()
-    pylab.savefig(args.save_path + "/visualize_matrices.png")
+    plt.savefig(args.save_path + "/visualize_matrices.png")
     logger.info("Figure \"visualize_matrices"
                 ".png\" saved at directory: " + args.save_path)
