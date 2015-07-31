66b8969868579eefda7be92fe37c185c3cb49b3a
Mohammad Pezeshki <mohammadpz@gmail.com>
2015-07-31 14:14:03 -0400

    file for residual rnn added

---
+++ rnn/build_model/build_model_residual.py
@@ -0,0 +1,68 @@
+import logging
+
+import theano
+from theano import tensor
+
+from rnn.build_model.build_model_utils import (get_prernn, get_presoft,
+                                               get_rnn_kwargs, get_costs,
+                                               get_rnn)
+
+floatX = theano.config.floatX
+logging.basicConfig(level='INFO')
+logger = logging.getLogger(__name__)
+
+
+def build_model_residual(args, dtype=floatX):
+    logger.info('Building model ...')
+
+    # Return list of 3D Tensor, one for each layer
+    # (Time X Batch X embedding_dim)
+    pre_rnn, x_mask = get_prernn(args)
+
+    rnn = get_rnn(args)
+
+    # Prepare inputs and initial states for the RNN
+    kwargs, inits = get_rnn_kwargs(pre_rnn, args)
+
+    # Apply the RNN to the inputs
+    h = rnn.apply(low_memory=True, mask=x_mask, **kwargs)
+
+    # We have
+    # h = [state, state_1, state_2 ...] if args.layers > 1
+    # h = state if args.layers == 1
+
+    # If we have skip connections, concatenate all the states
+    # Else only consider the state of the highest layer
+    last_states = {}
+    hidden_states = []
+    if args.layers > 1:
+        # Save all the last states
+        for d in range(args.layers):
+            # TODO correct bug
+            # h[d] = h[d] * x_mask
+            last_states[d] = h[d][-1, :, :]
+            h[d].name = "hidden_state_" + str(d)
+            hidden_states.append(h[d])
+        if args.skip_connections or args.skip_output:
+            h = tensor.concatenate(h, axis=2)
+        else:
+            h = h[-1]
+    else:
+        # TODO correct bug
+        # hidden_states.append(h * x_mask)
+        hidden_states.append(h)
+        hidden_states[0].name = "hidden_state_0"
+        # Note: if we have mask, then updating initial state
+        # with last state does not make sence anymore.
+        last_states[0] = h[-1, :, :]
+
+    # The updates of the hidden states
+    updates = []
+    for d in range(args.layers):
+        updates.append((inits[0][d], last_states[d]))
+
+    presoft = get_presoft(h, args)
+
+    cost, unregularized_cost = get_costs(presoft, args)
+
+    return cost, unregularized_cost, updates, hidden_states
